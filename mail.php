4c0b2d10b884f2b9f6bb307d30f142430bebe20f
zeidin1 <91205065+zeidin1@users.noreply.github.com>
2022-06-02 14:35:43 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -12,12 +12,12 @@ $messege = $_POST['text'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.outlook.live.com';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp-mail.outlook.com';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'copwebil2133@outlook.com'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = 'Arufir18'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 443; // TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->SMTPSecure = 'TLS';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('copwebil2133@outlook.com'); // от кого будет уходить письмо?
 $mail->addAddress('zeisin.a21@gmail.com');     // Кому будет уходить письмо 
